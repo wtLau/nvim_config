75612ae513a1250f35c431e4dc5c1c3eb4900616
Brian Lau <blau@appannie.com>
2023-05-24 15:08:43 -0700

    add default parsers for treesitter to install

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    -- Default parser to install for syntax highlight
+    ensure_installed = { "lua", "bash", "css", "html", "javascript", "jsdoc", "json", "python", "rust", "tsx",
+      "typescript", "vim" },
   },
 }
