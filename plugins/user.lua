33d027080c547efd0ff7b9ba0a8bacd24e666800
Brian Lau <blau@appannie.com>
2023-05-24 14:32:30 -0700

    add plugin for zk note taking

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,5 +8,39 @@ return {
   --   config = function()
   --     require("lsp_signature").setup()
   --   end,
-  -- },
+  {
+    -- https://github.com/mickael-menu/zk-nvim
+    "mickael-menu/zk-nvim",
+    config = function()
+      require("zk").setup({
+        -- See Setup section below
+        --
+        --
+        -- can be "telescope", "fzf" or "select" (`vim.ui.select`)
+        -- it's recommended to use "telescope" or "fzf"
+        picker = "fzf",
+
+        lsp = {
+          -- `config` is passed to `vim.lsp.start_client(config)`
+          config = {
+            cmd = { "zk", "lsp" },
+            name = "zk",
+            -- on_attach = ...
+            -- etc, see `:h vim.lsp.start_client()`
+          },
+
+          -- automatically attach buffers in a zk notebook that match the given filetypes
+          -- auto_attach = {
+          --   enabled = true,
+          --   filetypes = { "markdown" },
+          -- },
+        },
+      })
+    end
+  }
+  -- {
+  --
+  --   'wakatime/vim-wakatime',
+  --
+  -- }
 }
