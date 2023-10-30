daa3afe0f9d496f30bce76669e528e7d3d2a60aa
Brian <26396307+wtLau@users.noreply.github.com>
2023-10-30 16:10:36 -0400

    set auto wrap to true

--- options.lua
+++ options.lua
@@ -6,7 +6,7 @@ return {
     number = true,         -- sets vim.opt.number
     spell = false,         -- sets vim.opt.spell
     signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
-    wrap = false,          -- sets vim.opt.wrap
+    wrap = true,          -- sets vim.opt.wrap
     scrolloff = 80,        -- Number of lines to keep above and below the cursor
   },
   g = {
